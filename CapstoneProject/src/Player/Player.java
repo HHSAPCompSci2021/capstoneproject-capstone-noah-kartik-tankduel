ba0388a4d808e2609df3fa8664d2669053aed540
Noah Pien <npien766@student.fuhsd.org>
2022-05-10 12:47:45 -0700

    Update Player.java

--- CapstoneProject/src/Player/Player.java
+++ CapstoneProject/src/Player/Player.java
@@ -1,10 +1,56 @@
 package Player;
+import java.util.List;
 
 /**
  * the player in the game
  * @author Noah Pien and Kartik Joshi
+ * Credit to John Shelby for code
+ * @author npien
  *
  */
-public class Player {
+
+public class Player extends Sprite {
+
+	public static final int PLAYER_WIDTH = 40;
+	public static final int PLAYER_HEIGHT = 60;
+	private boolean onASurface;
+
+	private double xVel, yVel;
+
+	public Player(int x, int y) {
+		super(x, y, PLAYER_WIDTH, PLAYER_HEIGHT);
+		xVel = 0;
+		yVel = 0;
+		onASurface = false;
+	}
+
+	// METHODS
+	public void walk(int dir) {
+		xVel = dir * 3;
+	}
+
+	public void jump() {
+		if(onASurface) {
+		yVel -= 9;
+		}
+	}
+
+	public void act(List<Sprite> obstacles) {
+		onASurface = false;
+		yVel += 0.2;
+		
+		x += xVel;
+		y += yVel;
+		
+		for(Sprite s: obstacles) {
+			if(super.intersects(s)) {
+				yVel = 0;
+				super.y = s.y - super.height;
+				onASurface = true;
+			}
+		}
+		
+	}
+
 
 }
