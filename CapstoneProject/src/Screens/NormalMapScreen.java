7fbd7f09400e765e29543b000f70904fda63c77b
Noah <npien3@gmail.com>
2022-05-09 19:59:04 -0700

    update

--- CapstoneProject/src/Screens/NormalMapScreen.java
+++ CapstoneProject/src/Screens/NormalMapScreen.java
@@ -6,7 +6,7 @@ import System.DrawingSurface;
 
 public class NormalMapScreen extends Screens{
 	private DrawingSurface surface;
-	private Line2D l0,l1,l2,l3,l4,l5,l6,l7,l8,l9,l10,l11,l12,l13,l14,l15,l16,l17,l18,l19,l20,l21,l22,l23,l24,l25,l26,l27,l28,l29;
+	private Line2D l0,l1,l2,l3,l4,l5,l6,l7,l8,l9,l10,l11,l12,l13,l14,l15,l16,l17,l18,l19,l20,l21,l22,l23,l24,l25,l26,l27,l28,l29,l30,l31,l32,l33,l34,l35;
 	
 	
 	/* The area created by spawnX and spawnY will be the spawn area.
@@ -23,7 +23,7 @@ public class NormalMapScreen extends Screens{
 		super(1080, 720);
 		this.surface = surface;
 		// X:1080 by Y:720 range lines
-		l0 = new Line2D.Double (0,150,150,150);
+		l0 = new Line2D.Double (500,450,450,550);
 		l1 = new Line2D.Double (300,500,400,600);
 		l2 = new Line2D.Double (700,300,800,200);
 		l3 = new Line2D.Double (950,30,1050,130);
@@ -31,30 +31,38 @@ public class NormalMapScreen extends Screens{
 		l5 = new Line2D.Double (500, 70, 300, 70);
 		l6 = new Line2D.Double (780, 70, 1010,70);
 		l7 = new Line2D.Double (400,270,650,150);
-		l8 = new Line2D.Double (0,300,250,375);
-		l9 = new Line2D.Double (829,375,1079,300);
-		l10 = new Line2D.Double (0,550,230,550);
-		l11 = new Line2D.Double (849,550,1079,550);
+		l8 = new Line2D.Double (50,300,250,375);
+		l9 = new Line2D.Double (829,375,1050,300);
+		l10 = new Line2D.Double (50,550,230,550);
+		l11 = new Line2D.Double (849,550,1050,550);
 		l12 = new Line2D.Double (400,500,630,500);
-		l13 = new Line2D.Double (480, 360, 700,360);
+		l13 = new Line2D.Double (480, 360,700,360);
 		l14 = new Line2D.Double (600,620,800,450);
 		l15 = new Line2D.Double (250,250,350,350);
 		l16 = new Line2D.Double (200,500,500,300);
 		l17 = new Line2D.Double (650,0,700,50);
-		l18 = new Line2D.Double (320,720,480,640);
-		l19 = new Line2D.Double (600,720,500,590);
+		l18 = new Line2D.Double (320,690,480,640);
+		l19 = new Line2D.Double (600,690,500,590);
 		l20 = new Line2D.Double (100,250,250,150);
 		l21 = new Line2D.Double (150,0,250,100);
 		l22 = new Line2D.Double (350,150,350,250);
-		l23 = new Line2D.Double (2,2,2,2);
-		l24 = new Line2D.Double (2,2,2,2);
-		l25 = new Line2D.Double (2,2,2,2);
-		l26 = new Line2D.Double (2,2,2,2);
-		l27 = new Line2D.Double (2,2,2,2);
-		l28 = new Line2D.Double (2,2,2,2);
-		l29 = new Line2D.Double (2,2,2,2);
+		l23 = new Line2D.Double (670,100,770,150);
+		l24 = new Line2D.Double (720,125,750,105);
+		l25 = new Line2D.Double (0,400,70,450);
+		l26 = new Line2D.Double (40,600,100,660);
+		l27 = new Line2D.Double (850,170,950,270);
+		l28 = new Line2D.Double (1020,240,1080,180);
+		l29 = new Line2D.Double (790,570,880,680);
+		
+		l30 = new Line2D.Double (1000,680,1080,600);
+		l31 = new Line2D.Double (40,600,100,660);
+		l32 = new Line2D.Double (850,170,950,270);
+		l33 = new Line2D.Double (1020,240,1080,180);
+		l34 = new Line2D.Double (790,570,880,680);
+		l35 = new Line2D.Double (790,570,880,680);
 
-		platforms = new Line2D[] {l0,l1,l2,l3,l4,l5,l6,l7,l8,l9,l10,l11,l12,l13,l14,l15,l16,l17,l18,l19,l20,l21,l22,l23,l24,l25,l26,l27,l28,l29};
+
+		platforms = new Line2D[] {l0,l1,l2,l3,l4,l5,l6,l7,l8,l9,l10,l11,l12,l13,l14,l15,l16,l17,l18,l19,l20,l21,l22,l23,l24,l25,l26,l27,l28,l29,l30,l31,l32,l33,l34,l35};
 		
 		spawnX = new Line2D.Double (0,150,150,150);
 		spawnY = new Line2D.Double (150,0,150,150);
