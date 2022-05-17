4be33903900f807358123e3743c27db8ac645c07
Kartik Joshi <kjoshi980@student.fuhsd.org>
2022-05-17 13:02:45 -0700

    freeeze tag skeleton class

--- CapstoneProject/src/GameModes/FreezeMode.java
+++ CapstoneProject/src/GameModes/FreezeMode.java
@@ -6,5 +6,23 @@ package GameModes;
  *
  */
 public class FreezeMode extends GameModes{
-
+	private boolean frozen = false;
+	
+	public boolean frozen() {
+		//if intersected make them frozen make their velocity 0
+	}
+	
+	public boolean unfrozen() {
+		// if another runner touches them then they are unfrozen and their velocity is set to what the 
+		// keyboard presses
+	}
+	
+	public int tagged() {
+		// if a player has been tagged 3 times then the runners all lose
+	}
+	
+	public gameWon() {
+		// if when the time runs out and there are still runners who are not frozen or a person has
+		// not been tagged three times runner wins else taggers win
+	}
 }
