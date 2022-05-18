bab639eb55ea1f1b032f707d79dd9bd39d7f8018
Noah <npien3@gmail.com>
2022-05-18 00:05:25 -0700

    Update FreezeMode.java

--- CapstoneProject/src/GameModes/FreezeMode.java
+++ CapstoneProject/src/GameModes/FreezeMode.java
@@ -9,19 +9,23 @@ public class FreezeMode extends GameModes{
 	private boolean frozen = false;
 	
 	public boolean frozen() {
+		return frozen;
 		//if intersected make them frozen make their velocity 0
 	}
 	
 	public boolean unfrozen() {
+		return frozen;
 		// if another runner touches them then they are unfrozen and their velocity is set to what the 
 		// keyboard presses
 	}
 	
 	public int tagged() {
+		return 0;
 		// if a player has been tagged 3 times then the runners all lose
 	}
 	
-	public gameWon() {
+	public boolean gameWon() {
+		return frozen;
 		// if when the time runs out and there are still runners who are not frozen or a person has
 		// not been tagged three times runner wins else taggers win
 	}
