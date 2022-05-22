db45184aeb53dca3434d8bab68943d4894f44fa4
Varun Joshi <varun.joshi.2016.go@gmail.com>
2022-05-22 15:25:24 -0700

    cops and robbers basic implementation part 1

--- CapstoneProject/src/System/DrawingSurface.java
+++ CapstoneProject/src/System/DrawingSurface.java
@@ -85,7 +85,7 @@ public class DrawingSurface extends PApplet implements ScreenSwitcher {
 		MultiplayerOrNetwork multiNetwork = new MultiplayerOrNetwork(this);
 		screens.add(multiNetwork);
 		
-		CopsNRobbers copsNRobbers = new CopsNRobbers(this);
+		NormalMapCopsNRobbers copsNRobbers = new NormalMapCopsNRobbers(this);
 		screens.add(copsNRobbers);
 		activeScreen = screens.get(0);
 		
