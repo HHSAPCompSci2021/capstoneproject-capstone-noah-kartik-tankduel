0689b19b948666874789b91db5eb509b0310c67d
Noah Pien <npien766@student.fuhsd.org>
2022-05-23 13:03:41 -0700

    a

--- CapstoneProject/src/Screens/ServerNetworkHelp.java
+++ CapstoneProject/src/Screens/ServerNetworkHelp.java
@@ -1,9 +1,9 @@
 package Screens;
-
 import java.awt.Point;
 import java.awt.Rectangle;
 
 import System.DrawingSurface;
+import processing.core.PImage;
 /**
  * This screen shows the rules of the game
  * 
@@ -13,7 +13,16 @@ import System.DrawingSurface;
 public class ServerNetworkHelp extends Screens{
 
 	private Rectangle back;
+	private Rectangle next;
 	private DrawingSurface surface;
+	private PImage one;
+	private PImage two;
+	private PImage three;
+	private PImage four;
+	private PImage five;
+	private PImage six;
+	private PImage[] images;
+	private int imageNum;
 	/**
 	 * Gives each RulesScreen the default values
 	 * @param surface the DrawingSurface on which to draw on
@@ -22,6 +31,8 @@ public class ServerNetworkHelp extends Screens{
 		super(1080, 720);
 		this.surface = surface;
 		back = new Rectangle(20,690,70,20);
+		next = new Rectangle(990,690,70,20);
+		imageNum = 0;
 	}
 
 	/**
@@ -35,12 +46,45 @@ public class ServerNetworkHelp extends Screens{
 	
 			surface.pushStyle();
 			surface.textSize(70);
-			surface.text("How to start a server", 540-surface.textWidth("How to start a server")/2,70);
+			surface.text("How to start a server " + (imageNum + 1), 540-surface.textWidth("How to start a server")/2,70);
 			
+			
+			surface.image(images[imageNum], 25, 100,1030,600);
+
+			surface.rect(back.x, back.y, back.width, back.height);
+			surface.fill(0,0,0);
+			surface.textSize(20);
+			surface.text("BACK", back.x+back.width/2-surface.textWidth("BACK")/2, back.y + back.height/2 + 7);
+			
+			if(imageNum <5) {
+				surface.fill(255,255,255);
+				surface.rect(next.x, next.y, next.width, next.height);
+				surface.fill(0,0,0);
+				surface.textSize(20);
+				surface.text("NEXT", next.x+next.width/2-surface.textWidth("NEXT")/2, next.y + next.height/2 + 7);
+			}
+			surface.popStyle();
 		
-		
-		surface.popStyle();
-		
+	}
+	
+	/**
+	 * sets up the instruction images
+	 */
+	public void setup() {
+		images = new PImage[6];
+		one = surface.loadImage("one.png");
+		two = surface.loadImage("two.png");
+		three = surface.loadImage("three.png");
+		four = surface.loadImage("four.png");
+		five = surface.loadImage("five.png");
+		six = surface.loadImage("six.png");
+
+		images[0] = one;
+		images[1] = two;
+		images[2] = three;
+		images[3] = four;
+		images[4] = five;
+		images[5] = six;
 	}
 	
 	/**
@@ -48,9 +92,12 @@ public class ServerNetworkHelp extends Screens{
 	 */
 	public void mousePressed() {
 		Point p = surface.actualCoordinatesToAssumed(new Point(surface.mouseX,surface.mouseY));
-		if (back.contains(p))
+		if (back.contains(p) && imageNum == 0)
 			surface.switchScreen(ScreenSwitcher.RULES);
-		
+		if(back.contains(p)&& imageNum != 0)
+			imageNum--;
+		if(next.contains(p) && imageNum != 5)
+			imageNum++;
 		
 	}
 	
